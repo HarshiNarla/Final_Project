46767dfabedbcf7617a44b5dd829ca2ad46ec657
SivaHarshitha90 <ssivaharshitha90@gmail.com>
2024-11-08 14:51:49 +0530

    Update signupRoute.js

--- routes/signupRoute.js
+++ routes/signupRoute.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = express.Router();
 
-router.post('/signup', async (req, res) => {
+router.post('/', async (req, res) => {
   // Destructure the incoming request body
   const { firstName, lastName, signupEmail, signupPassword, phone, location, favoriteDestination } = req.body;
 
